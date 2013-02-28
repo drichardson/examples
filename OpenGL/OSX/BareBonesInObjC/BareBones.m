d9cdcd279576c8d5a69a533c0218aeee73a065a4
Doug Richardson <dougie.richardson@gmail.com>
2013-02-28 08:52:23 -0800

    make it work on 10.8 and use ARC

--- OpenGL/OSX/BareBonesInObjC/BareBones.m
+++ OpenGL/OSX/BareBonesInObjC/BareBones.m
@@ -1,27 +1,23 @@
 #import <Foundation/Foundation.h>
 #import <OpenGL/OpenGL.h>
 #import <Cocoa/Cocoa.h>
+#import <OpenGL/gl.h>
 
 int main (int argc, const char * argv[])
 {
-	NSAutoreleasePool* pool = [NSAutoreleasePool new];
-	
-	NSOpenGLPixelFormatAttribute attributes[] = {
-		0
-	};
-	
-	NSOpenGLPixelFormat* pixelFormat = [[NSOpenGLPixelFormat alloc] initWithAttributes:attributes];
-	NSOpenGLContext* context = [[NSOpenGLContext alloc] initWithFormat:pixelFormat shareContext:nil];
-	[pixelFormat release];
-	pixelFormat = nil;
-	
-	
-	[context makeCurrentContext];
-	printf("vendor is: %s\n", glGetString( GL_VENDOR ));
-	
-	[context release];
-	
-	[pool release];
+    @autoreleasepool {
+        NSOpenGLPixelFormatAttribute attributes[] = {
+            0
+        };
+        
+        NSOpenGLPixelFormat* pixelFormat = [[NSOpenGLPixelFormat alloc] initWithAttributes:attributes];
+        NSOpenGLContext* context = [[NSOpenGLContext alloc] initWithFormat:pixelFormat shareContext:nil];
+        pixelFormat = nil;
+        
+        
+        [context makeCurrentContext];
+        printf("vendor is: %s\n", glGetString( GL_VENDOR ));
+    }
 	
     return 0;
 }
