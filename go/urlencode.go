580207e854f4d568e3cf967b544b3852375c8549
Doug Richardson <dougie.richardson@gmail.com>
2013-04-08 20:09:22 -0700

    Updated for new version of go.

--- go/urlencode.go
+++ go/urlencode.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"http"
+	"net/url"
 )
 
 func main() {
 	fmt.Printf("Hi there %v\n", 88)
 	fmt.Println("String ", string(int64(88)), "lala")
-	fmt.Println(http.URLEscape("http://localhost:8080/?key1=val1&key2=val2"))
+	fmt.Println(url.QueryEscape("http://localhost:8080/?key1=val1&key2=val2"))
 }
