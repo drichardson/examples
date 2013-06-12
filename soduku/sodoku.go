82e007e64defb99de1bbb9ff9dd61682c23a349b
Doug Richardson <dougie.richardson@gmail.com>
2013-06-11 21:30:37 -0700

    Second version failure. Try to generate random board by selecting valid square one at a time. Problem is that I get myself into a hole by selecting values I need later on, thus running out of valid values before the entire board is generated.

--- soduku/sodoku.go
+++ soduku/sodoku.go
@@ -2,10 +2,11 @@ package main
 
 import (
     "fmt"
+    "sort"
 )
 
 func main() {
-    b := NewBoard()
+    b := NewRandomBoard()
     fmt.Printf("Board is:\n%v\n", b)
 }
 
@@ -30,6 +31,84 @@ func NewBoard() (b *Board) {
             }
         }
     }
+    return
+}
+
+func squareIndexForRowAndColumn(rowIndex uint, columnIndex uint) (uint) {
+    return rowIndex * 9 + columnIndex
+}
+
+func (b* Board) validValuesAtRowColumn(row uint, col uint) ([]int) {
+
+    // Assume all values are valid
+    validValues := make(map[int]bool, 9);
+    for i := 1; i <= 9; i++ {
+        validValues[i] = true
+    }
+
+
+    // Remove any values that already appear in the row
+    for i := uint(0); i < 9; i++ {
+        delete(validValues, int(b.grid[row * 9 + i]))
+    }
+
+    // Remove any values that already appear in the column
+    for i := uint(0); i < 9; i++ {
+        delete(validValues, int(b.grid[i * 9 + col]))
+    }
+
+    // Remove any values that already appear in the 3x3 group this square is part of
+    topLeftSquareInGroup := (row / 3) * 3 + (col / 3) * 3
+    fmt.Printf("Top left square is %v, %v, %v\n", topLeftSquareInGroup, (row / 3) * 3, (col / 3) * 3)
+    for iRow := uint(0); iRow < 3; iRow++ {
+        for iCol := uint(0); iCol < 3; iCol++ {
+            fmt.Printf("Removing square value %v because (%v, %v) = (%v, %v)\n", b.grid[iRow * 9 + iCol + topLeftSquareInGroup], iRow, iCol, iRow * 9, iCol + topLeftSquareInGroup)
+            delete(validValues, int(b.grid[iRow * 9 + iCol + topLeftSquareInGroup]))
+        }
+    }
+
+    // Turn the values map into an array of valid values.
+    values := make([]int, len(validValues))
+    counter := 0
+    for key, value := range validValues {
+        if value {
+            values[counter] = key
+            counter++
+        }
+    }
+
+    sort.Ints(values)
+    return values
+}
+
+func NewRandomBoard() (b* Board) {
+
+    b = new(Board)
+    b.grid = make([]uint, 9*9)
+
+    // For each row in the board
+    for rowIndex := uint(0); rowIndex < 9; rowIndex++ {
+
+        row := b.Row(rowIndex)
+
+        // For each column in the row
+        for col := uint(0); col < 9; col++ {
+
+            // Get an array of valid values
+            validValues := b.validValuesAtRowColumn(rowIndex, col)
+            fmt.Printf("Valid values for %v, %v = %v\n", rowIndex, col, validValues)
+
+            // Randomly select one
+            if len(validValues) == 0 {
+                fmt.Printf("Can't find value to select from for %v, %v for board\n%v\n", rowIndex, col, b)
+                panic("No valid values to select from")
+            }
+
+            row[col] = uint(validValues[0])
+        }
+
+    }
+
     return
 }
 
