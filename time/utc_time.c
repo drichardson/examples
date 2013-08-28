28d12163426e5e276541bda84d7d9d860333b21e
Doug Richardson <doug@upthere.com>
2013-08-28 11:01:12 -0700

    timegm without resorting to environment variable changes.

--- time/utc_time.c
+++ time/utc_time.c
@@ -19,11 +19,31 @@ static time_t fake_timegm(struct tm *t) {
     return mktime_in_zone(t, "UTC");
 }
 
-int main(int argc, char** argv) {
+static time_t fake_timegm_without_modifying_env(struct tm *t) {
+    struct tm zero_tm;
+    memset(&zero_tm, 0, sizeof(zero_tm));
+    zero_tm.tm_sec = 0;
+    zero_tm.tm_min = 0;
+    zero_tm.tm_hour = 0;
+    zero_tm.tm_mday = 1; // 1 based
+    zero_tm.tm_mon = 0; // 0 based
+    zero_tm.tm_year = 1970 - 1900; // 1900 based
+    zero_tm.tm_yday = 0; // 0 based
+    zero_tm.tm_isdst = 0;
+    zero_tm.tm_gmtoff = 0;
+
+    time_t offset_from_utc = mktime(&zero_tm);
+    time_t result = mktime(t);
+
+    return result - offset_from_utc;
+}
+
+static void test_time_string(char const* time_string, time_t expected) {
+    printf("*** Testing time %s, expected unix time %ld **********\n", time_string, expected);
     
     struct tm tm;
     memset(&tm, 0, sizeof(tm));
-    char const* rc = strptime("1970-01-01T00:00:00Z", "%FT%TZ", &tm);
+    char const* rc = strptime(time_string, "%FT%TZ", &tm);
     if (rc == NULL || *rc != '\0') {
         fprintf(stderr, "Failed to parse time string. Result %s\n", rc);
         exit(1);
@@ -38,6 +58,9 @@ int main(int argc, char** argv) {
     struct tm tm_for_fake = tm;
     time_t fake_time = fake_timegm(&tm_for_fake);
 
+    struct tm tm_for_fake_without_modifying_env = tm;
+    time_t fake_time_without_modifying_env = fake_timegm_without_modifying_env(&tm_for_fake_without_modifying_env);
+
     struct tm tm_for_gm = tm;
     time_t gm_time = timegm(&tm_for_gm);
 
@@ -48,7 +71,13 @@ int main(int argc, char** argv) {
     printf("mktime                = %ld\n", mktime_time);
     printf("mktime_in_zone EST    = %ld\n", mktime_in_zone_time);
     printf("fake_time             = %ld\n", fake_time);
+    printf("fake_time no modify   = %ld\n", fake_time_without_modifying_env);
     printf("gm_time               = %ld\n", gm_time);
-
+}
+int main(int argc, char** argv) {
+    test_time_string("1970-01-01T00:00:00Z", 0);
+    test_time_string("2013-08-28T17:58:53Z", 1377712733);
     return 0;
 }
+
+
