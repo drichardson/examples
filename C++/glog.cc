a5b80426acdc9075e6ba502c1adf9aa362517526
Doug Richardson <doug@upthere.com>
2014-03-21 15:07:53 -0700

    glog test

---
+++ C++/glog.cc
@@ -0,0 +1,18 @@
+#include <glog/logging.h>
+
+int main(int argc, char* argv[]) {
+    google::InitGoogleLogging(argv[0]);
+    FLAGS_minloglevel = google::GLOG_INFO;
+    FLAGS_logtostderr = true;
+    LOG(INFO) << "Found " << 123;
+    for(int i = 0; i < 100; ++i) {
+        LOG_EVERY_N(INFO, 10) << "Logging every 10 " << i;
+    }
+    LOG_IF(INFO, true) << "LOG_IF with true";
+    LOG_IF(INFO, false) << "LOG_IF with false";
+    for(int i = 0; i < 20; ++i) {
+        LOG_FIRST_N(INFO, 2) << "Logging first 2 " << i;
+    }
+    return 0;
+}
+
