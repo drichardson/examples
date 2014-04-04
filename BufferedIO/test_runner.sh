e84d602e149ebb299b75dc8ca7108d61cc7488e0
Doug Richardson <dougie.richardson@gmail.com>
2014-04-04 15:05:06 -0700

    Put tests in

--- BufferedIO/test_runner.sh
+++ BufferedIO/test_runner.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+set -e
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CMD="$DIR/io_test"
 
-NUMPROCESSES=1
-NUMTHREADS=4
-BUFSIZEBYTES=1024
-BYTESPERLOG=173
-TESTDURATION=5
-MODE=IOSTREAM
+function run_test {
+if [ -z "$1" ]; then
+    echo "No test name specified"
+    exit 1
+}
+if [ -e "$1" ]; then
+    echo "Test directory $1 already exists."
+    exit 1
+fi
+mkdir -p "$1"
+pushd "$1"
 
 for i in $(seq 1 $NUMPROCESSES)
 do
@@ -26,4 +33,39 @@ do
         echo "Process $pid return error"
     fi
 done
+
+popd
+
+}
+
+NUMPROCESSES=1
+NUMTHREADS=4
+BUFSIZEBYTES=0
+BYTESPERLOG=173
+TESTDURATION=5
+MODE=NONE
+
+run_test unbuffered
+MODE=FILE
+BUFSIZEBYTES=8000
+run_test Fbuffered8k
+BUFSIZEBYTES=80000
+run_test Fbuffered80k
+BUFSIZEBYTES=800000
+run_test Fbuffered800k
+BUFSIZEBYTES=8000000
+run_test Fbuffered8000k
+BUFSIZEBYTES=67108864
+run_test Fbuffered64M
+MODE=IOSTREAM
+BUFSIZEBYTES=8000
+run_test IObuffered8k
+BUFSIZEBYTES=80000
+run_test IObuffered80k
+BUFSIZEBYTES=800000
+run_test IObuffered800k
+BUFSIZEBYTES=8000000
+run_test IObuffered8000k
+BUFSIZEBYTES=67108864
+run_test IObuffered64M
 
