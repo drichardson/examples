0f57672b313113db6e275bf25a183f49455f432b
Doug Richardson <dougie.richardson@gmail.com>
2014-04-05 14:08:51 -0700

    Added some sleeps to see if they show up in gprof... they don't.

--- gprof/test.c
+++ gprof/test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 static void foo(int x)
 {
@@ -12,6 +13,7 @@ static void foo(int x)
 static void bar(int x_low, int x_high)
 {
     for(int i = x_low; i < x_high; ++i) {
+        if (i % 2000 == 0) sleep(1);
         if (i % 8 == 0) {
             puts("evenly divisible by 8");
         } else {
@@ -24,6 +26,7 @@ int main(int argc, char** argv)
 {
     foo(1);
     bar(1, 5);
+    bar(1000, 5000);
     return 0;
 }
 
