ed3736bc6f08d35c9522f7614ee5dff25862e49e
Doug Richardson <doug@upthere.com>
2014-05-29 09:40:04 -0800

    system_error test

---
+++ C++/system_error.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <system_error>
+
+void display(char const *prefix, std::exception const &e) {
+  std::cout << prefix << ' ' << e.what() << std::endl;
+}
+
+int main() {
+  display("EPIPE system_category",
+          std::system_error(EPIPE, std::system_category()));
+  display("EPIPE generic_category",
+          std::system_error(EPIPE, std::generic_category()));
+  return 0;
+}
