899764b75c8a59aea6c432dc89608bb73ce9d5a9
Doug Richardson <doug@upthere.com>
2014-05-29 15:50:52 -0800

    switches for stuff

--- C++/function.cc
+++ C++/function.cc
@@ -4,6 +4,9 @@
 #include <unistd.h>
 #include <sched.h>
 
+#define USE_SHAREDPTR 0
+#define USE_SET_AFFINITY 1
+
 inline uint64_t start_rdtsc(void)
 {
     uint32_t high, low;
@@ -42,17 +45,37 @@ std::function<void()> mything(int x)
     return [x] { printf("x is %d\n", x); };
 }
 
+#if USE_SHAREDPTR
 std::shared_ptr<std::function<void()>> global_f;
+#else
+std::function<void()> global_f;
+#endif
 
 void doGlobalF()
 {
+#if USE_SHAREDPTR
     (*global_f.get())();
+#else
+    global_f();
+#endif
 }
 
 
 
 int main(int argc, char** argv)
 {
+#if USE_SET_AFFINITY
+    printf("setting affinity\n");
+    cpu_set_t mask;
+    CPU_ZERO(&mask);
+    CPU_SET(0, &mask);
+    int rc = sched_setaffinity(getpid(), sizeof(mask), &mask);
+    if (rc == -1) {
+        printf("Couldn't set affinity: %d\n", errno);
+        exit(1);
+    }
+#endif
+
     std::function<void()> f;
 #if 0
     doSomething(foo);
@@ -73,12 +96,25 @@ int main(int argc, char** argv)
 
     f = mything(argc);
 
+#if USE_SHAREDPTR
     std::array<std::shared_ptr<std::function<void()>>, 198> fs;
+#else
+    std::array<std::function<void()>, 198> fs;
+#endif
+
     for(std::size_t i = 0; i < fs.size(); ++i) {
+#if USE_SHAREDPTR
         fs[i].reset(new std::function<void()>(mything(i)));
+#else
+        fs[i] = mything(i);
+#endif
     }
 
+#if USE_SHAREDPTR
     global_f.reset(new std::function<void()>(f));
+#else
+    global_f = f;
+#endif
     std::thread t(doGlobalF);
     t.join();
     fprintf(stdout, "Hi\n");
@@ -93,11 +129,19 @@ int main(int argc, char** argv)
 
     for(int i = 0; i < todo; ++i) {
         //printf("before\n");    
+
+#if USE_SHAREDPTR
         std::shared_ptr<std::function<void()>> f_ref = fs[i%fs.size()];
         uint64_t before = start_rdtsc();
         // global_f = f; // -O3 ~220, not printing before and after
         global_f = f_ref;
         uint64_t after = end_rdtsc();
+#else
+        std::function<void()> f = fs[i%fs.size()];
+        uint64_t before = start_rdtsc();
+        global_f = f; // -O3 ~220, not printing before and after
+        uint64_t after = end_rdtsc();
+#endif
         //printf("count: %lu\n", after - before);
         sum += (after - before);
     }
