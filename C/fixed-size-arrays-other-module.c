ff2ef6a5e60a629d426ac862c55ec9cc5a58efd5
Doug Richardson <dougie.richardson@gmail.com>
2014-09-25 01:49:06 -0700

    Fixed size array experiment.

---
+++ C/fixed-size-arrays-other-module.c
@@ -0,0 +1,11 @@
+
+int sum_3(char a[3]) {
+    return a[0] + a[1] + a[2];
+}
+
+int sum_4(char (*p)[4]) {
+    return (*p)[0] + (*p)[1] + (*p)[2] + (*p)[3];
+}
+
+
+
---
+++ C/fixed-size-arrays.c
@@ -0,0 +1,89 @@
+/**
+  * Summary
+  * Perform some compile time fixed size array experiments to see what warnings
+  * and errors compilers provide. Use gcc 4.9.1 and clang 3.4.2.
+  *
+  * Results: 
+  * 
+  * Experiments with fixed sized arrays using gcc (GCC) 4.9.1
+  * ---------------------------------------------------------------------------
+  * Calls to functions declared with the form typeX foo(typeY a[N]) are
+  * ONLY compile time checked for uninitialized values if the function
+  * definition is in the same module as the caller. However, array lengths are
+  * NOT checked. So if you provide an N+1 length array to the a[N] function
+  * above there will be no warning or error.
+  *
+  * Calls to functions declared with the form typeX foo(typeY (*p)[N]) will
+  * give you an incompatible pointer type error if a pointer to an array of a
+  * different type, dimention, or constness is used.
+  *
+  * Neither fixed sized form prevents you from exceeding the array bounds
+  * inside the function definition (unless the caller happens to be in the same
+  * module at which point you'll get an uninitialized value error.
+  *
+  * When using the pointer to array technique, the compiler will not
+  * automatically convert a non-const version to a const version.
+  * For example, given void foo(char const (*p)[4]), a call to it
+  * with foo(a), where char a[4] will fail with incompatible pointer type.
+  *
+  * Experiments with clang 3.4.2
+  * ---------------------------------------------------------------------------
+  * Similar results to gcc, with the exception that clang automatically
+  * converts from non-const to const types that are otherwise identical, which
+  * appears to be out of spec with C11 draft section 6.7.3 (Type qualifiers)
+  * paragraph that starts with "For two qualified types to be compatible..."
+  */
+#include <stdio.h>
+
+#if 1
+int sum_3(char a[3]);
+#else
+static int sum_3(char a[3]) {
+    return a[0] + a[1] + a[2];
+}
+#endif
+
+#if 1
+int sum_4(char const (*p)[4]);
+#else
+static int sum_4(char (*p)[4]) {
+    return (*p)[0] + (*p)[1] + (*p)[2] + (*p)[3] + (*p)[4] + (*p)[5];
+}
+#endif
+
+int main(int argc, char const** argv) {
+
+    // ******************************
+    // Test form typeX foo(typeY a[N])
+    // ******************************
+
+    // compile time error: excess delimiter char a[3] = { 1, 2, 4, 8 };
+    char a3[3] = { 1, 2, 4 };
+    printf("sum_3(a3) = %d\n", sum_3(a3));
+
+    char a4[4] = { 1, 2, 4, 8 }; 
+    // printf("sum_3(a4) = %d\n", sum_3(a4)); // no compile time failure here
+
+#if 0
+    // compile time failure when sum_3 is in same module as caller
+    // but *NO* compile time failure when sum_3 is in another module
+    char a2[2] = { 1, 2 };
+    printf("sum_3(a2) = %d\n", sum_3(a2));
+#endif
+
+
+    // **********************************
+    // Test form typeX foo(typeY (*p)[N])
+    // **********************************
+
+    printf("sum_4(&a4) = %d\n", sum_4((char const (*)[4])&a4));
+
+    // compile time error: incompatible pointer type printf("sum_4(&a3) = %d\n", sum_4(&a3));
+    // unsigned char u4[4] = { 1, 2, 4, 8};
+    // compile time error: incompatible pointer type printf("sum_4(&u4) = %d\n", sum_4(&u4));
+    char const c4[4] = { 1, 2, 4, 8 };
+    printf("sum_4(&c4) = %d\n", sum_4(&c4));
+
+    return 0;
+}
+
