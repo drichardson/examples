a1de6151a401d6ce4e9e48edd944ee82bae118a3
Doug Richardson <dougie.richardson@gmail.com>
2014-11-12 13:37:41 -0800

    typeid example

---
+++ C++/playground.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <typeinfo>
+
+using namespace std;
+
+int main(int const argc, char const* const* argv) {
+    typedef int myi;
+    using myi2 = int;
+    myi i1 = 123;
+    myi2 i2 = 456;
+    cout << "IDs: ";
+    cout << "int=" << typeid(int).name();
+    cout << ", i1=" << typeid(i1).name();
+    cout << ", i2=" << typeid(i2).name();
+    cout << ", myi2=" << typeid(myi2).name();
+    cout << endl;
+    return 0;
+}
