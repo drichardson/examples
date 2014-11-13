abbff355d969be0579c8764279e8e872ac54bfa3
Doug Richardson <dougie.richardson@gmail.com>
2014-11-13 15:27:11 -0800

    user defined literals

---
+++ C++/literals.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <boost/type_index.hpp>
+
+#define print_expr(expr) print_expr_internal(#expr, expr)
+
+template <typename T>
+void print_expr_internal(const char* msg, T&& value) {
+    using boost::typeindex::type_id_with_cvr;
+    std::cout << msg << " = " << value
+        << " of type " << type_id_with_cvr<T>().pretty_name()
+        << std::endl;
+}
+
+int main() {
+    using namespace std;
+    cout << "===== Various Bases for Ints ========" << endl;
+    print_expr(10);
+    print_expr(010);
+    print_expr(0x10);
+    print_expr(0X10);
+    print_expr(0b10);
+
+    cout << "====== ways of writing floats/doubles ======" << endl;
+    print_expr(10.0);
+    print_expr(1e1);
+
+    cout << "======= literal suffix to specify type =====" << endl;
+    print_expr(10);
+    print_expr(10UL);
+    print_expr(10Ul);
+    print_expr(10ul);
+    print_expr(10L);
+    print_expr(10l);
+    print_expr(0.0); // no f suffix means double
+    print_expr(0.0f);
+}
+
---
+++ C++/operators.cc
@@ -0,0 +1,71 @@
+#include <boost/type_index.hpp>
+#include <cstddef>
+#include <iostream>
+
+#define print_expr(expr) print_expr_internal(#expr, expr)
+
+template <typename T>
+void print_expr_internal(const char* msg, T&& value) {
+    using boost::typeindex::type_id_with_cvr;
+    std::cout << msg << " = " << value
+        << " of type " << type_id_with_cvr<T>().pretty_name()
+        << std::endl;
+}
+
+constexpr long double operator "" _K(long double v) {
+    return v * 1e3;
+}
+
+const long double operator "" _M(long double v) {
+    return v * 1e6;
+}
+
+struct unaligned {
+    char c1;
+    int i1;
+};
+
+struct alignedInt {
+    char c1;
+    alignas(sizeof(int)) int i1;
+};
+
+struct alignedIntx2 {
+    char c1;
+    alignas(sizeof(int)*2) int i1;
+};
+
+struct alignedIntx4 {
+    char c1;
+    alignas(sizeof(int)*4) int i1;
+};
+
+int main() {
+    // newer boolean and bitwise operator names.
+    print_expr(false and 1);
+    print_expr(false && 1);
+
+    print_expr(false or 1);
+    print_expr(false || 1);
+
+    print_expr(1 bitor 2);
+    print_expr(1 | 2);
+
+    print_expr(1 bitand 2);
+    print_expr(1 & 2);
+
+    // alignof and alignas
+    print_expr(alignof(unaligned));
+    print_expr(alignof(alignedInt));
+    print_expr(alignof(alignedIntx2));
+    print_expr(alignof(alignedIntx4));
+    print_expr(offsetof(unaligned, i1));
+    print_expr(offsetof(alignedIntx4, i1));
+    print_expr((size_t)&((alignedIntx4*)0)->i1); // my own offsetof for shits and giggles
+
+    // user defined literals
+    print_expr(2.0);
+    print_expr(2.0_K);
+    print_expr(2.0_M);
+}
+
