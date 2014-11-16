b2aaf99a0f52b6ae06b9fdedf96bb651be255199
Doug Richardson <dougie.richardson@gmail.com>
2014-11-16 15:39:16 -0800

    bigsort - sort ints generated by /dev/urandom with various algorithms. ifstream example.

---
+++ C++/bigsort.cc
@@ -0,0 +1,118 @@
+// a big sort, but still the size that can fit in memory.
+#include "sort.h"
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <functional>
+#include <iterator>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+
+template<typename T>
+void print_elements(T const & v) {
+    char const* space = "";
+    auto len = v.size();
+    for(decltype(len) i = 0; i < len; ++i) {
+        auto e = v[i];
+        cout << space << e;
+        space = " ";
+    }
+}
+
+template <typename Container, typename LessThan>
+struct sorting_algorithms
+{
+    using C = Container;
+    using L = LessThan;
+    template <typename E> using sorter_function = std::function<void(C&, L)>;
+    using value_type = decltype(((Container*)0)[0]);
+
+    template <typename E> struct named_algorithm {
+        const char* name;
+        sorter_function<E> function;
+    };
+
+    std::vector<named_algorithm<value_type>> values = {{
+        { "quicksort", &sort::quick_sort<C,L> },
+        { "insertion", &sort::insertion_sort<C,L> },
+        { "selection", &sort::selection_sort<C,L> },
+        { "bubble", &sort::bubble_sort<C, L> },
+        { "lucky", &sort::lucky_sort<C, L> },
+        { "stupid", &sort::stupid_sort<C, L> },
+    }};
+};
+
+template <typename Container, typename LessThan>
+void sort_and_report_for_each_algorithm(Container const & a, LessThan lt, ostream & out=cout) {
+
+    int comparisons = 0;
+    using E = decltype(a[0]);
+    auto cmp = [&comparisons, &lt](E const & a, E const & b) {
+        comparisons++;
+        return lt(a, b);
+    };
+
+    sorting_algorithms<Container, decltype(cmp)> algorithms;
+    for(auto const & algo : algorithms.values) {
+        Container a_copy(a);
+        comparisons = 0; // reset comparisons (incremented in cmp lambda above).
+
+        out << " * " << algo.name << "..." << flush;
+
+        auto start_time = chrono::high_resolution_clock::now();
+        // run sort
+        algo.function(a_copy, cmp);
+        auto end_time = chrono::high_resolution_clock::now();
+        
+        out << "complete. comparisons=" << static_cast<double>(comparisons)
+            << " duration="
+            << chrono::duration_cast<chrono::milliseconds>(end_time-start_time).count()
+            << "ms" << flush;
+
+        // verify sort is correct
+        bool sorted = true;
+        using index = decltype(a_copy.size());
+        for(index i = 1; i < a_copy.size(); ++i) {
+            if (lt(a_copy[i], a_copy[i-1])) {
+                sorted = false;
+                break;
+            }
+        }
+
+        // display status
+
+        if (!sorted) {
+            out << "ERROR: Algorithm failed to sort." << std::endl;
+            abort();
+        }
+
+        out << endl;
+    }
+}
+
+int lessthan(int a, int b) { return a < b; }
+
+int main(int argc, char* argv[]) {
+
+    double n = 1e3;
+
+    if (argc == 2) {
+        n = stod(argv[1]);
+    }
+
+    ifstream in("/dev/urandom");
+    vector<int> data(n);
+    cout << "data constructed\n";
+    for(auto & e : data) {
+        in.read(reinterpret_cast<char*>(&e), sizeof(e));
+    }
+    cout << "data read\n";
+    cout << "n=" << n 
+        << ", lg(n)=" << log2(n)
+        << ", n*lg(n)=" << n*log2(n)
+        << ", n^2=" << n*n
+        << "\n";
+    sort_and_report_for_each_algorithm(data, lessthan);
+}
