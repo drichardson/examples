aee077ae24b8bab1b191b588571268af37d26c19
Doug Richardson <dougie.richardson@gmail.com>
2014-12-28 11:08:32 -0800

    make dynamic programming example take command line parameter

--- C++/rod_cutting.cc
+++ C++/rod_cutting.cc
@@ -1,7 +1,5 @@
 // from introduction to algorithms 3rd edition 15.1
 
-#define USE_DYNAMIC_PROGRAMMING 0
-
 #include <iostream>
 #include <vector>
 #include <cassert>
@@ -18,17 +16,21 @@ static unsigned num_max_prices_calculated;
 static unsigned cut_rod_internal(
         std::vector<LengthPrice> const & prices,
         unsigned RodLength,
-        std::vector<unsigned> & max_prices
+        std::vector<unsigned> & max_prices,
+        bool use_dynamic_programming
         )
 {
-#if USE_DYNAMIC_PROGRAMMING
-    if (max_prices[RodLength] > 0) return max_prices[RodLength];
-#endif
+    if (use_dynamic_programming && max_prices[RodLength] > 0) return max_prices[RodLength];
 
     unsigned max_price = 0;
     for(auto price : prices) {
         if (price.length < RodLength) {
-            unsigned p = price.price + cut_rod_internal(prices, RodLength-price.length, max_prices);
+            unsigned p = price.price +
+                cut_rod_internal(prices,
+                        RodLength-price.length,
+                        max_prices,
+                        use_dynamic_programming);
+
             if (p > max_price) {
                 max_price = p;
             }
@@ -38,35 +40,53 @@ static unsigned cut_rod_internal(
     }
 
     ++num_max_prices_calculated;
-#if USE_DYNAMIC_PROGRAMMING
     max_prices[RodLength] = max_price;
-#endif
     return max_price;
 }
 
 static unsigned cut_rod(
         std::vector<LengthPrice> const & prices,
-        unsigned RodLength)
+        unsigned RodLength,
+        bool use_dynamic_programming)
 {
     std::vector<unsigned> max_prices;
     max_prices.resize(RodLength+1);
-    return cut_rod_internal(prices, RodLength, max_prices);
+    return cut_rod_internal(prices, RodLength, max_prices, use_dynamic_programming);
 }
 
-int main() { 
+int main(int argc, char const* argv[]) { 
     using std::cerr;
     using std::cout;
+
+    if (argc < 2) {
+        cerr << "Usage: rod_cutting <use_dynamic>\n"
+            << "  0 - don't use dynamic programming\n"
+            << "  1 - use dynamic programming lookup table\n";
+        return 1;
+    }
+
+    bool use_dynamic_programming = false;
+    switch(argv[1][0]) {
+    case '0':
+        use_dynamic_programming = false;
+        break;
+    case '1':
+        use_dynamic_programming = true;
+        break;
+    default:
+        cerr << "Expected 0 or 1 but got " << argv[1][0] << '\n';
+        return 1;
+    }
 
     vector<LengthPrice> prices{
         {1,1}, {2,5}, {3,8}, {4,9}, {5,10}, {6,17}, {7,17}, {8,20}, {9,24}, {10,30}
     };
 
     for(unsigned length = 1; length < 30; ++length) {
-        unsigned value = cut_rod(prices, length);
+        unsigned value = cut_rod(prices, length, use_dynamic_programming);
         cout << "Length=" << length << " Value=" << value << '\n';
     }
 
-    cout << "USE_DYNAMIC_PROGRAMMING: " << USE_DYNAMIC_PROGRAMMING << '\n';
     cout << "Number of times max price calculated: " << num_max_prices_calculated << '\n';
 }
 
