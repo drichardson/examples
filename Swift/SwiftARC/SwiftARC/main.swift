dbdfd331798df2c84ddd4ef1747717227b363fa9
Doug Richardson <dougie.richardson@gmail.com>
2015-02-20 20:06:05 -0800

    self nilling block example

--- Swift/SwiftARC/SwiftARC/main.swift
+++ Swift/SwiftARC/SwiftARC/main.swift
@@ -12,11 +12,11 @@ class A {
     let name : String
     init(_ name : String) {
         self.name = name
-        println("init \(name)")
+        println("------- init \(name)   ----------")
     }
     
     deinit {
-        println("deinit \(name)")
+        println("------- deinit \(name) ----------")
     }
 }
 
@@ -37,7 +37,7 @@ func optionalClosureContainingA() {
     c?()
 }
 
-func optionalClosureContainingAsyncA() {
+func optionalClosureContainingAsyncA(doneCallback : dispatch_block_t) {
     var a = A(__FUNCTION__)
     var c : (() -> ())? = nil
     
@@ -57,6 +57,7 @@ func optionalClosureContainingAsyncA() {
         println("setting c to nil")
         c = nil // A deinit called here
         println("c set to nil")
+        dispatch_async(dispatch_get_main_queue(), doneCallback)
     })
     
     c = {
@@ -66,11 +67,56 @@ func optionalClosureContainingAsyncA() {
     println("end " + __FUNCTION__)
 }
 
+func selfNillingOptionalClosureContainingAsyncA(doneCallback : dispatch_block_t) {
+    var a = A(__FUNCTION__)
+    var c : (() -> ())? = nil
+    
+    let now = dispatch_time(DISPATCH_TIME_NOW, 0)
+    let firstBlockTime = dispatch_time(now, Int64(2 * NSEC_PER_SEC))
+    let secondBlockTime = dispatch_time(firstBlockTime, Int64(2 * NSEC_PER_SEC))
+    
+    dispatch_after(firstBlockTime, dispatch_get_main_queue(), {
+        println("first. is nil? \(c == nil)")
+        c?()
+        return
+    })
+    
+    dispatch_after(secondBlockTime, dispatch_get_main_queue(), {
+        println("second. is nil? \(c == nil)")
+        while c != nil {
+            c?()
+        }
+        println("second ending")
+    })
+    
+    var counter = 3
+    c = {
+        println("c called with a = \(a)")
+        --counter
+        if counter <= 0 {
+            println("counter zero, setting c = nil")
+            c = nil // appears that invoker retains block, so setting to nil here
+            // does not result in an immediate deinit, which is good considering we're still
+            // executing the block.
+            println("c set to nil")
+            dispatch_async(dispatch_get_main_queue(), doneCallback)
+        } else {
+            println("counter decremented to \(counter)")
+        }
+        
+        println("exiting call to c closure")
+    }
+    
+    println("end " + __FUNCTION__)
+}
+
 let globalA = A("global")
 localA()
 optionalA()
 optionalClosureContainingA()
-optionalClosureContainingAsyncA()
+optionalClosureContainingAsyncA {
+    selfNillingOptionalClosureContainingAsyncA { exit(0) }
+    }
 
 dispatch_main()
 
