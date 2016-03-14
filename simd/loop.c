4b0fa4f147c13ae4ba699f0fbb46126fe5da39c5
Doug Richardson <dougie.richardson@gmail.com>
2016-03-14 15:34:23 -0700

    restrict versions

--- simd/loop.c
+++ simd/loop.c
@@ -1,10 +1,22 @@
-void variable_iteration_loop(float *a, float *b, float fac, float c, int n) {
+void variable_iteration_loop(float* a, float* b, float fac, float c, int n) {
     for(int i = 0; i < n; ++i) {
         a[i] = fac * b[i] + c;
     }
 }
 
-void fixed100_iteration_loop(float *a, float *b, float fac, float c) {
+void variable_iteration_loop_restrict(float* restrict a, float* restrict b, float fac, float c, int n) {
+    for(int i = 0; i < n; ++i) {
+        a[i] = fac * b[i] + c;
+    }
+}
+
+void fixed100_iteration_loop(float* a, float* b, float fac, float c) {
+    for(int i = 0; i < 100; ++i) {
+        a[i] = fac * b[i] + c;
+    }
+}
+
+void fixed100_iteration_loop_restrict(float* restrict a, float* restrict b, float fac, float c) {
     for(int i = 0; i < 100; ++i) {
         a[i] = fac * b[i] + c;
     }
