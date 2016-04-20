f499f0425b6254b3b6b0daa9aaf83502ea56cae2
Doug Richardson <dougie.richardson@gmail.com>
2016-04-20 13:17:44 -0700

    demonstrate passing template function as template parameter to another template function.

--- C++/templates.cc
+++ C++/templates.cc
@@ -22,11 +22,36 @@ public:
     }
 };
 
+template <class T>
+T mymax(T a, T b)
+{
+    return a > b ? a : b;
+}
+
+template <class T>
+T mymin(T a, T b)
+{
+    return a < b ? a : b;
+}
+
+template <class T, T f(T,T) = mymin>
+void print_result(T a, T b)
+{
+    std::cout << "print_result: a=" << a << ", b=" << b << ", result=" << f(a,b) << "\n";
+}
+
 int main()
 {
     Derived<int> d{12};
     d.foo();
     decltype(d) d2{11};
     d2.foo();
+
+    std::cout << "defaulting to mymin\n";
+    print_result(1,2);
+    print_result(3,2);
+    std::cout << "explicit mymax\n";
+    print_result<int, mymax>(1,2);
+    print_result<int, mymax>(3,2);
 }
 
