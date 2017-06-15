8e435ccf60bf26937f363fdc2c1409971e79f21a
Doug Richardson <doug@caffeine.tv>
2017-06-15 13:12:07 -0700

    tail recursive list length

--- Erlang/tut4.erl
+++ Erlang/tut4.erl
@@ -1,9 +1,15 @@
 % Tutorial 4 from http://www.erlang.org/doc/getting_started/seq_prog.html
 -module (tut4).
--export ([list_length/1]).
+-export ([list_length/1, list_length_tr/1]).
 
 % Note: this implementation of list_length is not tail recursive.
 list_length([]) ->
     0;
-list_length([First | Rest]) ->
+list_length([_ | Rest]) ->
     1 + list_length(Rest).
+
+% Note: this implementation of list_lenght_tr is tail recursive.
+list_length_tr(L) -> list_length_tr(L, 0).
+
+list_length_tr([], Acc) -> Acc;
+list_length_tr([_|Rest], Acc) -> list_length_tr(Rest, Acc+1).
