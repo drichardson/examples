8899bffc7a32c00f713f80234f1695cd3aab2c17
Doug Richardson <dougie.richardson@gmail.com>
2018-02-04 20:30:53 -0800

    .net core test

---
+++ .net/README.md
@@ -0,0 +1,1 @@
+Examples from [Demystifying .NET Core and .NET Standard](https://msdn.microsoft.com/en-us/magazine/mt842506.aspx).
---
+++ .net/hello/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using logic;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What's your name: ");
+            var name = Console.ReadLine();
+            var message = HelloWorld.GetMessage(name);
+            Console.WriteLine(message);
+        }
+    }
+}
