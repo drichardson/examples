c87c6b5a66bb367b12793f51c46596f5204725bf
Doug Richardson <doug@rekt.email>
2018-09-17 14:48:30 -0700

    python test

--- python/c_api/embedded-python.cc
+++ python/c_api/embedded-python.cc
@@ -1,10 +1,20 @@
 #include "Python.h"
+
 #include <string>
+#include <iostream>
 
 using namespace std::string_literals;
+using std::cout;
+using std::endl;
 
 int main(int argc, char** argv) {
-  Py_Initialize();
+  ::Py_Initialize();
+
+  int rc = ::PyRun_SimpleStringFlags("print([x**2 for x in range(10)])", NULL);
+  cout << "Result: " << rc << endl;
+
+  rc = ::PyRun_SimpleStringFlags("BLAH BLAH", NULL);
+  cout << "Result: " << rc << endl;
 
   return 0;
 }
