b5b6a470cf71d958c77e9136427296ccd4456e72
Doug Richardson <doug@rekt.email>
2019-09-17 17:14:18 -0700

    placement new stack example

--- C++/placement_new.cc
+++ C++/placement_new.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <type_traits>
 
 using namespace std;
 
@@ -15,14 +17,98 @@ public:
     }
 };
 
+struct NoCopy
+{
+    NoCopy() {}
+    NoCopy(const NoCopy&) = delete;
+    NoCopy& operator=(const NoCopy&) = delete;
+};
+
+struct MyItem : NoCopy
+{
+    MyItem() = delete;
+    MyItem(int id, const char* str) : id(id), str(str) { cout << "MyItem(" << id << ")\n"; }
+    ~MyItem() { cout << "~MyItem(" << id << ")\n"; }
+
+    int id;
+    std::string str;
+};
+
+class MyStack : NoCopy
+{
+    static constexpr int MaxItems = 10;
+    std::aligned_storage<sizeof(MyItem), alignof(MyItem)>::type data[MaxItems];
+    std::size_t count;
+
+    MyItem* get(int index) { return reinterpret_cast<MyItem*>(data) + index; }
+    MyItem const* get(int index) const { return reinterpret_cast<MyItem const*>(data) + index; }
+public:
+
+    MyStack() : count(0) {}
+
+    ~MyStack()
+    {
+        while(count > 0)
+        {
+            pop();
+        }
+    }
+
+    void push(int id, const char* str)
+    {
+        if (count + 1 >= MaxItems)
+        {
+            cout << "ERROR: NO MORE SPACE\n";
+            return;
+        }
+
+        new(get(count)) MyItem(id, str);
+        count++;
+    }
+
+    const MyItem& top() const
+    {
+        return *get(count-1);
+    }
+
+    void pop()
+    {
+        count--;
+        get(count)->~MyItem();
+    }
+
+    std::size_t size() const { return count; }
+};
+
 int main() {
-    A a{1, 'a'};
-    unsigned char* blob[10000];
-    int next {0};
-    A* p = new(blob+next) A{2, 'b'};
-    next += sizeof(A);
-    A* p2 = new(blob+next) A{3, 'c'};
-    p->~A();
-    p2->~A();
+
+    // basics
+    cout << "basics\n\n";
+    {
+        A a{1, 'a'};
+        unsigned char* blob[10000];
+        int next {0};
+        A* p = new(blob+next) A{2, 'b'};
+        next += sizeof(A);
+        A* p2 = new(blob+next) A{3, 'c'};
+        p->~A();
+        p2->~A();
+    }
+
+
+    // my own stack built using placement new and explicit destructor calling.
+    cout << "\nmy stack\n\n";
+    {
+        MyStack s;
+        s.push(1, "one");
+        s.push(2, "two");
+        s.push(3, "three");
+
+        while(s.size() > 0)
+        {
+            cout << "top is: " << s.top().id << ", " << s.top().str << "\n";
+            s.pop();
+        }
+    }
 }
 
