cef3a2ac75d610515209128f9ec2baaaec7b05d5
Doug Richardson <doug@rekt.email>
2019-09-20 12:44:09 -0700

    wip

--- network/ephemeral.c
+++ network/ephemeral.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 int main()
 {
@@ -66,13 +67,39 @@ int main()
         exit(1);
     }
     struct sockaddr_in* addr = (struct sockaddr_in*)&storage;
-    in_port_t port = ntohs(addr->sin_port);
+    in_port_t client_port = ntohs(addr->sin_port);
 
-    printf("Client was allocated port %d\n", port);
+    printf("Client was allocated port %d\n", client_port);
 
     //
-    // Now make a server listen
+    // Now try to listen on the same port that was just allocated to a client.
     //
+    int server = socket(AF_INET, SOCK_STREAM, 0);
+    if (server == -1) {
+        fprintf(stderr, "socket failed for server with %d: %s\n", errno, strerror(errno));
+        exit(1);
+    }
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(client_port);
+    server_addr.sin_addr.s_addr = addr->sin_addr.s_addr; // listen only on the same address the client connect to example.com from
+    if (bind(server, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        fprintf(stderr, "bind failed for server. %d: %s\n", errno, strerror(errno));
+        exit(1);
+    }
+
+    if (listen(server, 3) == -1) {
+        fprintf(stderr, "listen failed for server. %d: %s\n", errno, strerror(errno));
+        exit(1);
+    }
+
+    printf("Server able to bind to client port. This is most unexpected.\n");
+
+    printf("Sleeping\n");
+    sleep(300);
+    printf("Done sleeping\n");
 
     return 0;
 }
