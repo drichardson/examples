2e45aea3e620b177ad984aeff39ed8f88260d244
Doug Richardson <doug@rekt.email>
2020-02-03 17:28:10 -0800

    wip

--- kubernetes/application/helloworld/hello.go
+++ kubernetes/application/helloworld/hello.go
@@ -5,20 +5,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 var address = flag.String("address", ":8080", "Listen address.")
 var counter int
 
+var serverUUID uuid.UUID
+
 func main() {
 	flag.Parse()
 	log.Print("Starting hello application")
+	serverUUID, err := uuid.NewRandom()
+	if err != nil {
+		panic(err)
+	}
+	log.Print("Server UUID:", serverUUID)
 	http.HandleFunc("/", index)
 	log.Fatal(http.ListenAndServe(*address, nil))
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	log.Printf("index handler, counter: %d", counter)
-	w.Write([]byte(fmt.Sprintf("Hello, World!\nCounter: %d\n", counter)))
+	log.Printf("serverUUID: %v, counter: %d", serverUUID, counter)
+	w.Write([]byte(fmt.Sprintf("Hello, World!\nServer UUID: %v\nCounter: %d\n", serverUUID, counter)))
 	counter++
 }
