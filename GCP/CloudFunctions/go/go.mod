6c5158404308d7ab37c3213cd56154d5d3dba3c4
Doug Richardson <doug@rekt.email>
2020-02-29 17:15:35 -0800

    structured logging comparison test

--- GCP/CloudFunctions/go/go.mod
+++ GCP/CloudFunctions/go/go.mod
@@ -1,3 +1,11 @@
 module github.com/drichardson/examples/cloudfunctions
 
 go 1.13
+
+require (
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/inconshreveable/log15 v0.0.0-20200109203555-b30bc20e4fd1
+	github.com/mattn/go-colorable v0.1.6 // indirect
+	github.com/rs/zerolog v1.18.0
+	github.com/sirupsen/logrus v1.4.2
+)
--- GCP/CloudFunctions/go/structured_logging.go
+++ GCP/CloudFunctions/go/structured_logging.go
@@ -3,9 +3,13 @@ package cloudfunctions
 
 import (
 	"fmt"
+	"github.com/inconshreveable/log15"
+	zlog "github.com/rs/zerolog/log"
+	"github.com/sirupsen/logrus"
 	"html"
 	"log"
 	"net/http"
+	"os"
 )
 
 func StructuredLoggingEndpoint(w http.ResponseWriter, r *http.Request) {
@@ -14,6 +18,7 @@ func StructuredLoggingEndpoint(w http.ResponseWriter, r *http.Request) {
 	twoLineMessage := `Two line
 message`
 
+	log.Println("=== log Package ===")
 	log.Println("Println", oneLineMessage)
 	log.Println("Println", twoLineMessage)
 
@@ -22,6 +27,45 @@ message`
 
 	log.Printf("Printf: %s", oneLineMessage)
 	log.Printf("Printf: %s", twoLineMessage)
+
+	log.Println("=== sirupsen/logrus Package ===")
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	logrus.SetOutput(os.Stdout)
+	logrus.SetLevel(logrus.DebugLevel)
+
+	logrus.Debug("Debug Basic")
+	logrus.Info("Info Basic")
+	logrus.Warn("Warn Basic")
+	logrus.Error("Error Basic")
+	logrus.Info(oneLineMessage)
+	logrus.Info(twoLineMessage)
+	logrus.WithFields(logrus.Fields{
+		"myIntField":             123,
+		"myStringField":          "howdy",
+		"myMultilineStringField": twoLineMessage,
+	}).Info("With Fields Example")
+
+	log.Println("=== inconshreveable/log15 Package ===")
+	l15 := log15.New()
+	l15.SetHandler(log15.StreamHandler(os.Stdout, log15.JsonFormat()))
+	l15.Debug("Debug Basic")
+	l15.Info("Debug Basic")
+	l15.Warn("Debug Basic")
+	l15.Error("Debug Basic")
+	l15.Crit("Crit Basic")
+	l15.Info(oneLineMessage)
+	l15.Info(twoLineMessage)
+	l15.Info("With Fields Example", "myIntField", 532, "myStringField", "howdy", "myMultilineStringField", twoLineMessage)
+
+	log.Println("=== rs/zerolog Package ===")
+	zlog.Trace().Msg("Trace Basic")
+	zlog.Debug().Msg("Debug Basic")
+	zlog.Info().Msg("Debug Basic")
+	zlog.Warn().Msg("Debug Basic")
+	zlog.Error().Msg("Debug Basic")
+	zlog.Info().Msg(oneLineMessage)
+	zlog.Info().Msg(twoLineMessage)
+	zlog.Info().Int("myIntField", 532).Str("myStringField", "howdy").Str("myMultilineStringField", twoLineMessage).Msg("With Fields Example")
 
 	fmt.Fprint(w, html.EscapeString("OK"))
 }
