e16f8e9adc292d1e83d365b10395cc95deacb381
Doug Richardson <doug@rekt.email>
2020-03-29 20:02:53 -0700

    compute integration sums onces, not over and over, to improve perf

--- python/scipy/calculus.py
+++ python/scipy/calculus.py
@@ -51,6 +51,37 @@ def nth_integral_from_0(f, dx, n):
         f = integral_from_0(f, dx)
     return f
 
+# Return an array of integrals evaluated over the ranges of [ar[i], ar[i+1]]. The
+# result is an array with len(ar)-1 elements.
+def integrate_array(f, dx, ar):
+    a = 0
+    b = 1
+    sums = np.zeros(len(ar)-1)
+    while b < len(ar):
+        sums[a] = integrate(f, dx, ar[a], ar[b])
+        a = b
+        b += 1
+    return sums
+
+# Integrate an array of x positions ar. ar[0] must be 0 and progressive
+# entries in the array must move farther from 0. That is, abs(ar[i]) < abs(ar[i+1]).
+def integrate_array_from_0(f, dx, ar):
+    # integrate_array computes the area under the curve of [ar[i],ar[i+1]],
+    # which results in len(sums) = len(ar) - 1...
+    sums = integrate_array(f, dx, ar)
+
+    # ...accumulated_sums on the other hand tells you the area under the curve from
+    # [0,ar[i]].
+    accumulated_sums = np.zeros(len(sums)+1)
+    accumulated_sums[0] = 0
+    for i in range(0,len(sums)):
+        accumulated_sums[i+1] = sums[i] + accumulated_sums[i]
+    return accumulated_sums
+
+
+#def nth_integrate_array_from_0(f, dx, ar, n):
+    #sums = integrate_array(f, dx, ar)
+
 # Plot derivatives and integrals of f to order.
 def plot(f, order):
 
@@ -62,7 +93,7 @@ def plot(f, order):
         return "C{}".format(color)
 
     DX=0.01
-    x = np.linspace(0, np.pi * 2 * 4, 80)
+    x = np.linspace(0, np.pi * 2 * 4, 200)
     #x = np.arange(0, 5, DX)
 
     fig, ax = plt.subplots()
@@ -72,6 +103,9 @@ def plot(f, order):
     def plot(f, label):
         ax.plot(x, list(map(f, x)), color=nextcolor(), label=label)
 
+    def plotCalculated(y, label):
+        ax.plot(x, y, color=nextcolor(), label=label)
+
     plot(f, 'f')
 
     # Derivatives
@@ -80,16 +114,11 @@ def plot(f, order):
 
     # Integrals
     for n in range(1, order+1):
-        plot(nth_integral_from_0(f, DX, n), '∫')
+        plotCalculated(integrate_array_from_0(f, DX, x), '∫' * n)
+        #plot(nth_integral_from_0(f, DX, n), '∫')
 
     plt.title('f = {}'.format(f.__name__))
     plt.legend()
-
-    #jfig, ax = plt.subplots()
-    #my_plotter(ax, x, x)
-    #plt.title('again')
-    #plt.legend()
-
 
 #
 # Functions to be analyzed
@@ -111,8 +140,8 @@ plot(constant, 1)
 plot(linear, 1)
 plot(quadratic, 2)
 plot(exponential, 2)
-plot(math.sin, 2)
-plot(math.cos, 2)
+plot(math.sin, 3)
+plot(math.cos, 3)
 
 plt.show()
 
