ca6e0014e8c9f20c3aeff55cfa379f483ab7968c
Doug Richardson <doug@rekt.email>
2020-04-02 20:59:58 -0700

    'wip'

--- Windows/DirectoryListingPerformance/FindFile.c
+++ Windows/DirectoryListingPerformance/FindFile.c
@@ -1,30 +1,32 @@
+#define UNICODE
+#define _UNICODE
+#define WIN32_LEAN_AND_MEAN
+#define WINVER _WIN32_WINNT_WIN10
+
 #include <windows.h>
-#include <tchar.h>
 #include <stdio.h>
 
-void _tmain(int argc, TCHAR *argv[])
+void wmain(int argc, wchar_t* argv[])
 {
     WIN32_FIND_DATA FindFileData;
     HANDLE hFind;
 
-    if( argc != 2 )
+    if (argc != 2)
     {
-        _tprintf(TEXT("Usage: %s [target_file]\n"), argv[0]);
+        wprintf(L"Usage: %ls [target_file]\n", argv[0]);
         return;
     }
 
-    _tprintf (TEXT("Target file is %s\n"), argv[1]);
-    hFind = FindFirstFileEx(argv[1], FindExInfoStandard, &FindFileData,
-            FindExSearchNameMatch, NULL, 0);
+    wprintf(L"Target file is %ls\n", argv[1]);
+    hFind = FindFirstFileExW(argv[1], FindExInfoStandard, &FindFileData, FindExSearchNameMatch, NULL, 0);
     if (hFind == INVALID_HANDLE_VALUE) 
     {
-        printf ("FindFirstFileEx failed (%d)\n", GetLastError());
+        wprintf(L"FindFirstFileEx failed (%d)\n", (int)GetLastError());
         return;
     } 
     else 
     {
-        _tprintf (TEXT("The first file found is %s\n"), 
-                FindFileData.cFileName);
+        wprintf(L"The first file found is %ls\n", FindFileData.cFileName);
         FindClose(hFind);
     }
 }
