4f9910dfe5148a448f621cb98f65fc109ec73014
Doug Richardson <git@rekt.email>
2020-12-02 20:36:16 -0800

    fix

--- DSP/BookExamples/6-convolution.h
+++ DSP/BookExamples/6-convolution.h
@@ -1,12 +1,3 @@
-/*
- *  table-6-1-convolution.h
- *  BookExamples
- *
- *  Created by Doug on 3/27/08.
- *  Copyright 2008 Douglas Richardson. All rights reserved.
- *
- */
-
 #ifndef _INPUT_CONVOLUTIONS_H_
 #define _INPUT_CONVOLUTIONS_H_
 
@@ -15,38 +6,40 @@
 typedef struct
 {
 	unsigned sample_count;
-	int32_t samples[1];
-	
+	int32_t samples[];
+
 } Int32Signal;
 
-Int32Signal* Int32Signal_Alloc(unsigned sample_count);
+Int32Signal *Int32Signal_Alloc(unsigned sample_count);
 void Int32Signal_Free(Int32Signal *signal);
 
 // x: the input signal
 // h: the impulse response
 // return: the output signal (normally called y)
-Int32Signal* Int32Signal_ConvolveUsingInputSideAlgorithm_AllocResult(const Int32Signal *x, const Int32Signal *h);
+Int32Signal *
+Int32Signal_ConvolveUsingInputSideAlgorithm_AllocResult(const Int32Signal *x,
+							const Int32Signal *h);
 
-void Int32Signal_Print(const Int32Signal* s);
-
+void Int32Signal_Print(const Int32Signal *s);
 
 typedef struct
 {
 	unsigned sample_count;
-	float samples[1];
-	
+	float samples[];
+
 } Float32Signal;
 
-Float32Signal* Float32Signal_Alloc(unsigned sample_count);
+Float32Signal *Float32Signal_Alloc(unsigned sample_count);
 void Float32Signal_Free(Float32Signal *signal);
 
 // x: the input signal
 // h: the impulse response
 // return: the output signal (normally called y)
-Float32Signal* Float32Signal_ConvolveUsingInputSideAlgorithm_AllocResult(const Float32Signal *x, const Float32Signal *h);
-Float32Signal* Float32Signal_ConvolveUsingOutputSideAlgorithm_AllocResult(const Float32Signal *x, const Float32Signal *h);
+Float32Signal *Float32Signal_ConvolveUsingInputSideAlgorithm_AllocResult(
+    const Float32Signal *x, const Float32Signal *h);
+Float32Signal *Float32Signal_ConvolveUsingOutputSideAlgorithm_AllocResult(
+    const Float32Signal *x, const Float32Signal *h);
 
-void Float32Signal_Print(const Float32Signal* s);
-
+void Float32Signal_Print(const Float32Signal *s);
 
 #endif
