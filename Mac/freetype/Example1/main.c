0180b3cf8c096e9c68f852967d896aea4cf5e091
Doug Richardson <git@rekt.email>
2020-12-02 20:38:31 -0800

    cleanup

--- Mac/freetype/Example1/main.c
+++ Mac/freetype/Example1/main.c
@@ -1,34 +1,32 @@
-#include <stdio.h>
-#include <ft2build.h>
 #include <freetype2/freetype/freetype.h>
+#include <ft2build.h>
+#include <stdio.h>
 
 #define kFontFilename "/Library/Fonts/Times New Roman.ttf"
 
-static void err(const char* msg);
+static void err(const char *msg);
 
-int main (int argc, const char * argv[])
+int main(int argc, const char *argv[])
 {
-	
 	FT_Library library;
-	
+
 	FT_Error error = FT_Init_FreeType(&library);
-	if(error)
+	if (error)
 		err("freetype Library initialization error");
-	
-	
+
 	puts("Going to load font from " kFontFilename);
 	FT_Face face;
 	error = FT_New_Face(library, kFontFilename, 0, &face);
-	
-	if(error)
+
+	if (error)
 		err("Couldn't load font");
-	
+
 	printf("This font has %d faces\n", face->num_faces);
-	
-    return 0;
+
+	return 0;
 }
 
-static void err(const char* msg)
+static void err(const char *msg)
 {
 	fprintf(stderr, "ERROR: %s\n", msg);
 	exit(1);
