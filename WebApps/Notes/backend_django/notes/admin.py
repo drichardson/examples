cfc586c954b66568ad2fa7d87a8070615cb5eb55
Doug Richardson <git@rekt.email>
2020-12-22 10:33:12 -0800

    remove old cruft

--- WebApps/Notes/backend_django/notes/admin.py
+++
@@ -1,4 +0,0 @@
-from backend_django.notes.models import Note
-from django.contrib import admin
-
-admin.site.register(Note)
--- WebApps/Notes/backend_django/notes/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('backend_django.notes.views',
-    (r'^json_notes', 'json_notes'),
-)
