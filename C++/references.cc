3abda57b467301c8d649de6ddaabbc77e18533b9
Doug Richardson <git@rekt.email>
2021-01-19 17:35:52 -0800

    references wip

--- C++/references.cc
+++ C++/references.cc
@@ -1,44 +1,99 @@
 #include <iostream>
 #include <utility>
 
+using std::cout;
+using std::endl;
+
 #if 0
 // error: if defined, get call of overloaded is ambiguous
 void foo(int x) {
-    std::cout << "int" << std::endl;
+	std::cout << "int" << std::endl;
 }
 #endif
 
-void foo(const int& x) {
-    std::cout << "const int& " << x << std::endl;
+void foo(const int &x)
+{
+	cout << "const int& " << x << endl;
 }
 
-void foo(int& x) {
-    std::cout << "int& " << x << std::endl;
+void foo(int &x)
+{
+	cout << "int& " << x << endl;
 }
 
-void foo(int&& x) {
-    std::cout << "int&& " << x << std::endl;
+void foo(int &&x)
+{
+	cout << "int&& " << x << endl;
 }
 
-int main() {
-    int y = 123;
+class X {
+	char const *s;
 
-    // int&&
-    foo(y);
+public:
+	X(char const *str) : s(str) { cout << "X(): " << s << endl; }
 
-    int& yref = y;
-    foo(yref);
+	// Rule of five for testing
+	// https://en.cppreference.com/w/cpp/language/rule_of_three
 
-    // int&&
-    foo(123);
-    foo(std::move(y));
-    foo(static_cast<int&&>(y));
+	// destructor
+	~X() { cout << "~X(): " << s << endl; }
 
-    // const int&
-    foo(const_cast<const int&>(y));
-    const int& ycref = y;
-    foo(ycref);
+	// copy constructor
+	X(X const &other) : s(other.s) { cout << "X(X const&): " << s << endl; }
 
-    return 0;
+	// move constructor
+	X(X &&other) : s(std::exchange(other.s, nullptr))
+	{
+		cout << "X(X&&): " << s << endl;
+	}
+
+	// copy assignment
+	X &operator=(X const &other)
+	{
+		*this = X(other);
+		cout << "operator=(X const&): " << s << endl;
+		return *this;
+	}
+
+	// move assignment
+	X &operator=(X &&other)
+	{
+		std::swap(s, other.s);
+		cout << "operator=(X&&): " << s << endl;
+		return *this;
+	}
+};
+
+X xfoo()
+{
+	X x("xfoo");
+	return x;
 }
 
+int main()
+{
+	int y = 123;
+
+	// int&&
+	foo(y);
+
+	int &yref = y;
+	foo(yref);
+
+	// int&&
+	foo(123);
+	foo(std::move(y));
+	foo(static_cast<int &&>(y));
+
+	// const int&
+	foo(const_cast<const int &>(y));
+	const int &ycref = y;
+	foo(ycref);
+
+	X x("1");
+	x = xfoo();
+	X x2("2");
+	x2 = x;
+
+	return 0;
+}
