c2e086d9a0a07df8ac7b873c9cb387b77ca2eeea
Doug Richardson <git@rekt.email>
2021-02-03 15:42:55 -0800

    more initialization examples

--- C++/initialization.cc
+++ C++/initialization.cc
@@ -2,13 +2,32 @@
 
 using namespace std;
 
-int main() {
-    unsigned int i = 0xdeadbeef;
-    cout << "i is " << hex << i << endl;
+int main()
+{
+	cout << hex;
 
-    // empty initializier list. Using placement new
-    // so that I know what the memory is set to.
-    decltype(i)* pi = new(&i) decltype(i){};
-    cout << "pi is " << *pi << endl;
+	unsigned int i;
+	auto reset = [&i]() {
+		i = 0xdeadbeef;
+		cout << "reset i to " << i << endl;
+	};
+
+	// empty initializier list. Using placement new
+	// so that I know what the memory is set to.
+	reset();
+	cout << "i is: " << i << endl;
+	new (&i) int{};
+	cout << "after empty initialization list: " << i << endl;
+
+	reset();
+	new (&i) int{1};
+	cout << "after non-empty initialization list: " << i << endl;
+
+	reset();
+	new (&i) int();
+	cout << "after default initialization: " << i << endl;
+
+	reset();
+	new (&i) int(2);
+	cout << "after explicit initizliation: " << i << endl;
 }
-
