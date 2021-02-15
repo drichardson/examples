7d4af75c6c8e37e46ffa68168af35d311c9145cd
Doug Richardson <git@rekt.email>
2021-02-15 13:47:30 -0800

    other forward declares

--- C++/template_forward_decl.cc
+++ C++/template_forward_decl.cc
@@ -12,6 +12,8 @@ class B {
 public:
 	// forward declaration okay because only used as pointer in Template
 	A<class C> a_c_forward;
+	A<struct S> a_s_forward;
+	A<union U> a_u_forward;
 };
 
 class C
@@ -21,9 +23,25 @@ public:
 	int i;
 };
 
+struct S
+{
+	S() : i(555) {}
+	int i;
+};
+
+union U
+{
+	U() : i(3) {}
+	int i;
+	int j;
+};
+
 int main()
 {
 	B b;
-	std::cout << "i is " << b.a_c_forward.p->i << std::endl;
+	std::cout << "c i is " << b.a_c_forward.p->i << std::endl;
+	std::cout << "s i is " << b.a_s_forward.p->i << std::endl;
+	std::cout << "u i is " << b.a_u_forward.p->i << std::endl;
+	std::cout << "u j is " << b.a_u_forward.p->j << std::endl;
 	return 0;
 }
