6a10ec802b298bdb5b2b06517e566c997326f33e
Doug Richardson <doug@whatnot.com>
2022-01-24 18:46:50 -0800

    wip

--- Erlang/programming_erlang/afile_client.erl
+++ Erlang/programming_erlang/afile_client.erl
@@ -1,6 +1,7 @@
 -module(afile_client).
 -export([run/0]).
 
+
 run() ->
 	Server = afile_server:start("."),
 	Server ! {self(), list_dir},
@@ -8,12 +9,16 @@ run() ->
 		{_, {ok, Items}} -> io:format("Got directories: ~s", [string:join(Items, " ")])
 	end,
 	io:format("1~n"),
-	Server ! {self(), get_file, "afile_client.erl"},
+	Filename = "Makefile.BAD",
+	Server ! {self(), get_file, Filename},
 	io:format("2~n"),
 	receive
-		{_, {ok, Body}} -> io:format("CONTENT~n~s", [Body]);
-		_ -> io:format("OTHER~n")
+		{Pid, {ok, Body}} -> io:format("CONTENT from ~w~n====~s BEGIN~n~s====~s END~n",
+					       [Pid, Filename, Body, Filename]);
+		{Pid, {error, Reason}} -> io:format("FAILURE from ~w: ~w~n", Pid, Reason);
+		Err -> io:format("OTHER ~w~n", [Err])
 	end,
-	io:format("3~n").
+	io:format("3~n"),
+	init:stop().
 
 
