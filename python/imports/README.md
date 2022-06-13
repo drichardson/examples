e0ff0f50100975a5bcb8e9ef8c80217ae5dd852a
Doug Richardson <doug@whatnot.com>
2022-06-13 09:42:26 -0700

    update to python3

--- python/imports/README.md
+++ python/imports/README.md
@@ -1,7 +1,7 @@
 # Import System Example
 
 Demonstrates absolute and relative imports, and the differences of running a
-pythong script directly and as a module.
+python script directly and as a module.
 
    python as_script/imports.py
    python -m as_module
--- python/threads.py
+++ python/threads.py
@@ -1,32 +1,32 @@
 from time import sleep
-from thread import start_new_thread
+from _thread import start_new_thread
 
 def _1HzThread(p1, p2):
 	while 1:
-		print "1Hz: p1=" + str(p1) + ", p2=" + p2
+		print("1Hz: p1=" + str(p1) + ", p2=" + p2)
 		sleep(1)
 		
 def _2HzThread():
 	while 1:
-		print "2Hz"
+		print("2Hz")
 		sleep(0.5)
 		
 def _10HzThread():
 	while 1:
-		print "10Hz"
+		print("10Hz")
 		sleep(0.1)
 		
 
-print "Starting threads"
+print("Starting threads")
 start_new_thread(_1HzThread, ("Arg 1", "Arg 2"))
 start_new_thread(_2HzThread, ())
 start_new_thread(_10HzThread, ())
 
-print "Sleeping"
+print("Sleeping")
 
 while 1:
 	sleep(100)
 	
-print "Ending"
+print("Ending")
 		
 
