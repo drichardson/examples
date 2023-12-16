36430246c98162f35cf0a6a786ff31bc83c044c4
Doug Richardson <git@rekt.email>
2023-12-16 13:11:24 -0800

    state example

--- rust/webservers/try-actix/src/main.rs
+++ rust/webservers/try-actix/src/main.rs
@@ -14,13 +14,27 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+struct AppState {
+    app_name: String,
+}
+
+#[get("/state/app_name")]
+async fn get_app_name(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name;
+    format!("Hello {app_name}!")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix Web"),
+            }))
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
+            .service(get_app_name)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
