b1ed9680288ef071227b12607db2cc76721b1f27
Doug Richardson <git@rekt.email>
2023-12-16 13:18:42 -0800

    mutable state

--- rust/webservers/try-actix/src/main.rs
+++ rust/webservers/try-actix/src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Mutex;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -24,17 +25,41 @@ async fn get_app_name(data: web::Data<AppState>) -> String {
     format!("Hello {app_name}!")
 }
 
+struct AppStateWithCounter {
+    counter: Mutex<i32>, // mutex used to mutate safely across threads
+}
+
+#[get("/state/increment")]
+async fn increment(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
+    format!("Request number: {counter}")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    /*
+    HttpServer accepts an application factory rather than an application instance. An
+    HttpServer constructs an application instance for each thread. Therefore, application data
+    must be constructed multiple times. If you want to share data between different threads, a
+    shareable object should be used, e.g. Send + Sync.
+    */
+
+    let counter = web::Data::new(AppStateWithCounter {
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(AppState {
                 app_name: String::from("Actix Web"),
             }))
+            .app_data(counter.clone())
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
             .service(get_app_name)
+            .service(increment)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
