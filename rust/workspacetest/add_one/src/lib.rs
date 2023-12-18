ad924ed84b322e47f2559e2f9c7fd6ba37ef1dd3
Doug Richardson <git@rekt.email>
2023-12-17 16:10:48 -0800

    wip

---
+++ rust/workspacetest/add_one/src/lib.rs
@@ -0,0 +1,20 @@
+use rand;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
---
+++ rust/workspacetest/adder/src/main.rs
@@ -0,0 +1,6 @@
+use rand;
+
+fn main() {
+    let num = 10;
+    println!("{num} plus one is {}", add_one::add_one(num));
+}
